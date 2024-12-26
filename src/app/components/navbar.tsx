f99d0e0c4ebbfaa451e08f5884cf4fd1e4bbf50e
MRizainF <icon.firdaus@gmail.com>
2024-12-26 15:16:20 +0700

    style: changing bg for nav to transparent

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -6,27 +6,27 @@ const Navbar = () => {
     return (
         <nav className='bg-transparent mx-auto max-w-2xl'>
             <div className='flex justify-center items-center gap-[70px] p-5 font-normal'>
-                <div className='text-base duration-300 hover:bg-gray-50 hover:px-1 hover:py-2 hover:rounded-[5px]'>
+                <div className='text-base duration-300 hover:bg-transparent hover:px-1 hover:py-2 hover:rounded-[5px]'>
                     <Link href='/#home'>
                         Home
                     </Link>
                 </div>
-                <div className='text-base duration-300 hover:bg-gray-50 hover:px-1 hover:py-2 hover:rounded-[5px]'>
+                <div className='text-base duration-300 hover:bg-transparent hover:px-1 hover:py-2 hover:rounded-[5px]'>
                     <Link href='/#projects'>
                         Projects
                     </Link>
                 </div>
-                <div className='text-base duration-300 hover:bg-gray-50 hover:px-1 hover:py-2 hover:rounded-[5px]'>
+                <div className='text-base duration-300 hover:bg-transparenthover:px-1 hover:py-2 hover:rounded-[5px]'>
                     <Link href='/#techstacks'>
                         Tech Stacks
                     </Link>
                 </div>
-                <div className='text-base duration-300 hover:bg-gray-50 hover:px-1 hover:py-2 hover:rounded-[5px]'>
+                <div className='text-base duration-300 hover:bg-transparent hover:px-1 hover:py-2 hover:rounded-[5px]'>
                     <Link href='/#phase'>
                         Process
                     </Link>
                 </div>
-                <div className='text-base duration-300 hover:bg-gray-50 hover:px-1 hover:py-2 hover:rounded-[5px]'>
+                <div className='text-base duration-300 hover:bg-transparent hover:px-1 hover:py-2 hover:rounded-[5px]'>
                     <Link href='/#contact'>
                         Contact
                     </Link>
